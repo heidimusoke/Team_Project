ae6331c99cb3a4a5d06a41f2265f7e89536d431a
hmusoke <heidimusoke@gmail.com>
2026-02-22 21:38:55 +0000

    Edit flight method created

--- connectDB.py
+++ connectDB.py
@@ -1,4 +1,8 @@
+from flask import Flask, render_template, request, redirect
 import sqlite3
+
+app = Flask(__name__)   # <-- THIS must come before any @app.route
+
 def getDbConnection():
     conn = sqlite3.connect("AirPlaneSystem.db")
     conn.row_factory = sqlite3.Row
@@ -11,11 +15,36 @@ def showFlights():
     conn.close()
 
 
-@app.route("/editFlight")
-def editFlight():
+@app.route("/editFlight/<int:flightID>", methods=["GET", "POST"])
+def editFlight(flightID):
+    conn = getDbConnection()
+
+    if request.method == "POST":
+        dept = request.form["Departure"]
+        dest = request.form["Destination"]
+        deptDate = request.form["Departure Date"]
+        deptTime = request.form["Departure Time"]
+        arrDate = request.form["Arrival Date"]
+        arrTime = request.form["Arrival Time"]
+        numSeats = request.form["Number of seats"]
+        airlineID = request.form["Airline ID"]
+
+        conn.execute("""UPDATE Flight
+                     SET departure = ?, destination = ?, departureDate = ?, departureTime = ?, arrivalDate = ?, arrivalTime = ?, numberSeats =?, airlineID = ?  WHERE flightNumber = ?""", (dept, dest, deptDate, deptTime, arrDate, arrTime, numSeats, airlineID, flightID))
+        conn.commit()
+        conn.close()
+        return redirect("/showFlight")
+
+        # GET REQUEST FOR THE FORM
+    flight = conn.execute("SELECT * FROM Flight WHERE flightNumber =?", (flightID,)).fetchone()
+    conn.close()
+    return render_template("editFlight.html", flight=flight)        
+
 
 
 @app.route("/addFlight")
 def addFlight():
     conn=getDbConnection()
-    conn.
+    conn.close()
+
+print(editFlight(2))
